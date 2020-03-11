8f63958c68a8e56002f700457cdd79d7800357f3
Sofia <sof9999@mail.ru>
2020-03-11 22:25:54 +0300

    Final test

--- 01/test.cpp
+++ 01/test.cpp
@@ -1,19 +1,34 @@
-#include <cstdio>
 #include <cstdlib>
+#include <cstdio>
 
 #include "makeallocator.h"
 
 int main()
 {
-	makeAllocator(30);
-	char* some1 = alloc(20);
-	printf("%p\n", some1);
-	char* some2 = alloc(20);
-	printf("%p\n", some2);
-	reset();
-	char* some3 = alloc(30);
-	printf("%p\n", some3);
-	reset();
-	free(start);
+	const int ALLSIZE = 10000;
+	
+	makeAllocator(ALLSIZE);
+	char* pointer;
+	
+	for(int i=1; i<ALLSIZE; i++)
+	{
+		for(int j=0; j<(ALLSIZE / i); j++)
+		{
+			pointer = alloc(i);
+			if(pointer == nullptr)
+			{
+				printf("Something went wrong...\n");
+				return 1;
+			}
+		}
+		pointer = alloc(i);
+		if(pointer != nullptr)
+		{
+			printf("It's too good...\n");
+			return 2;
+		}
+		reset();
+	}
+	printf("Everything is OK!\n");
 	return 0;
 }
