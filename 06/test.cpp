286ae08daa451e3c85612f50caaef9b4c1c11ebf
Sofia <sof9999@mail.ru>
2020-04-16 16:08:36 +0300

    Add 06 homework

--- 06/test.cpp
+++ 06/test.cpp
@@ -8,5 +8,14 @@ int main()
 	auto text = format("{1}+{1} = {0}", 2, "one");
 	assert(text == "one+one = 2");
 	std::cout << text << std::endl;
+	try
+	{
+		auto text1 = format("{2}", 55);
+	}
+	catch(std::runtime_error& ex)
+	{
+		std::cout << "runtime error catched" << std::endl;
+		std::cout << ex.what() << std::endl;
+	}
     return 0;
 }
